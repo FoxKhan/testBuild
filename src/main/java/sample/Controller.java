e1a34e7e9ba971d5e4c2dc8fa8bdad23958053c3
Aleksey Kislov <aleksey_kislov@epam.com>
2018-11-05 19:24:46 +0300

    merge

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -1,12 +1,19 @@
 package sample;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
-import javafx.scene.control.PasswordField;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import sample.model.pojo.KeyProperty;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -14,70 +21,70 @@ import static sample.ProConstanse.STORE_FOLDER;
 
 public class Controller implements Initializable {
 
-    public ListView<String> keyStoreList;
-    public ListView<String> keyList;
-    public ListView aboutList;
-    public PasswordField masterPass;
-    public Button btn1;
-    public Button btn2;
-    public Button btn3;
-    public Button btn4;
-    public Button btn5;
-    public Button btn6;
+  public ListView<String> keyStoreList;
+  public ListView<String> keyList;
+  public ListView aboutList;
+  public PasswordField masterPass;
+  public Button btn1;
+  public Button btn2;
+  public Button btn3;
+  public Button btn4;
+  public Button btn5;
+  public Button btn6;
 
-    private final static String ZIPA = "zipalign -v -p 4 c:/users/artk5/documents/testapplication/app/build/outputs/apk/release/app-release-unsigned.apk c:/app/app-release-unsigned-aligned.apk";
-    private final static String PATH_TO_ZIPA = "C:\\Users\\nika-\\AppData\\Local\\Android\\Sdk\\build-tools\\27.0.3\\";
+  private final static String ZIPA = "zipalign -v -p 4 c:/users/artk5/documents/testapplication/app/build/outputs/apk/release/app-release-unsigned.apk c:/app/app-release-unsigned-aligned.apk";
+  private final static String PATH_TO_ZIPA = "C:\\Users\\nika-\\AppData\\Local\\Android\\Sdk\\build-tools\\27.0.3\\";
 
 
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        loadKeyStores();
-        initListKeyStore();
+  @Override
+  public void initialize(URL url, ResourceBundle resourceBundle) {
+    loadKeyStores();
+    initListKeyStore();
+  }
+
+  private void initListKeyStore() {
+
+    keyStoreList.getSelectionModel().selectedItemProperty().addListener(((observableValue, s, t1) -> {
+
+      Commands.getAliasList(t1, masterPass.getText())
+              .doOnSuccess(strings -> keyList.getItems().addAll(strings))
+              .subscribe();
+
+      Commands.getSHAList(t1, masterPass.getText())
+              .doOnSuccess(strings -> {
+                strings.toString();//todo
+              })
+              .subscribe();
+    }));
+  }
+
+  private void loadKeyStores() {
+
+    File keyStoresFolder = new File(STORE_FOLDER);
+    if (!keyStoresFolder.isDirectory()) return;
+
+    File[] keyStores = keyStoresFolder.listFiles();
+
+    keyStoreList.getItems().clear();
+
+    if (keyStores != null) {
+      for (File keyStore : keyStores) {
+        keyStoreList.getItems().add(keyStore.getName());
+      }
     }
-
-    private void initListKeyStore() {
-
-        keyStoreList.getSelectionModel().selectedItemProperty().addListener(((observableValue, s, t1) -> {
-
-            Commands.getAliasList(t1, masterPass.getText())
-                    .doOnSuccess(strings -> keyList.getItems().addAll(strings))
-                    .subscribe();
-
-            Commands.getSHAList(t1, masterPass.getText())
-                    .doOnSuccess(strings -> {
-                       strings.toString();//todo
-                    })
-                    .subscribe();
-        }));
-    }
-
-    private void loadKeyStores() {
-
-        File keyStoresFolder = new File(STORE_FOLDER);
-        if (!keyStoresFolder.isDirectory()) return;
-
-        File[] keyStores = keyStoresFolder.listFiles();
-
-        keyStoreList.getItems().clear();
-
-        if (keyStores != null) {
-            for(File keyStore: keyStores){
-                keyStoreList.getItems().add(keyStore.getName());
-            }
-        }
-    }
+  }
 
 
-    public void genKey(ActionEvent actionEvent) {
-        Commands.addKeyStore("123456", "simpleName","key0", "Max Boyar", "Moscow", "RU");
+  public void genKey(ActionEvent actionEvent) {
+    Commands.addKeyStore("123456", "simpleName", "key0", "Max Boyar", "Moscow", "RU");
 //        System.out.println("genKey");
 //        testCmd();
-    }
+  }
 
 
-    public void genAlias(ActionEvent actionEvent) {
-        Commands.addKeyStore("123456", "simpleName","key1", "Max Boyar", "Moscow", "RU");
-    }
+  public void genAlias(ActionEvent actionEvent) {
+    Commands.addKeyStore("123456", "simpleName", "key1", "Max Boyar", "Moscow", "RU");
+  }
 
   public void addKey() {
     try {
@@ -97,7 +104,8 @@ public class Controller implements Initializable {
       FieldController controller = loader.getController();
       controller.setKeyProperty(keyProperty);
       dialogStage.showAndWait();
-      keyStoreList.getItems().add(keyProperty);
+      //keyStoreList.getItems().add(keyProperty.toString());
+      //TODO Добавляет в первый список
     } catch (IOException e) {
       e.printStackTrace();
     }
@@ -108,7 +116,16 @@ public class Controller implements Initializable {
     alert.setTitle("SHA - 1");
     VBox dialogPaneContent = new VBox();
     TextArea textArea = new TextArea();
-    textArea.setText("ТУТ ТВОЙ КОД");
+    Commands.getSHAList("simpleName.jks", masterPass.getText())
+            .doOnSuccess(strings -> {
+              StringBuilder stringBuilder = new StringBuilder();
+              for (String str: strings) {
+                stringBuilder.append(str).append("\n");
+              }
+              textArea.setText(stringBuilder.toString());
+            })
+            .subscribe();
+
 
     dialogPaneContent.getChildren().addAll(textArea);
     alert.getDialogPane().setContent(dialogPaneContent);
